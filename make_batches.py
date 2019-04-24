ca7151aa44dd785fe7094d04b923f97fb7c32b80
bhattg <iitdgantavya@gmail.com>
2019-04-24 16:46:54 +0530

    some changes

--- make_batches.py
+++ make_batches.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 def make_batch(x, y, batch_size=32):
     l = []
     batch_list=[]
@@ -8,10 +9,17 @@ def make_batch(x, y, batch_size=32):
         l.append((x[i], y[i]))
     j=0
     random.shuffle(l)
+    x_list = []; y_list=[]
+    for i in range(len(l)):
+    	x, y = l[i]
+    	x_list.append(x)
+    	y_list.append(y)
+    x = np.asarray(x_list)
+    y = np.asarray(y_list)
     while j<m:
         if j+batch_size < m:
-            batch_list.append(l[j:j+batch_size])
+            batch_list.append((x[j:j+batch_size], y[j:j+batch_size]))
         else:
-            batch_list.append(l[j:])
+            batch_list.append((x[j:], y[j:]))
         j+=batch_size
     return batch_list
